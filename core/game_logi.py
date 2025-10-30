c228970c2dbe640871a34108090c56212c117ecb
menahem_erlich <erlichmenahem@gmail.com>
2025-10-30 10:02:04 +0200

    game_logic

--- core/game_logi.py
+++ core/game_logi.py
@@ -1,9 +1,35 @@
 
 def calculate_hand_value(hand: list[dict]) -> int:
-    pass
+    sum_hand = 0
+    for i in hand:
+        if i["rank"] == "A":
+            sum_hand += 1
+        elif i["rank"] == "J" or i["rank"] == "Q" or i["rank"] == "K":
+            sum_hand += 10
+        else:
+            sum_hand += int(i["rank"])
+    return sum_hand
+
+
 
 def deal_two_each(deck: list[dict], player: dict, dealer: dict) -> None:
-    pass
+    for i in range(2):
+        card = deck.pop(0)
+        player["hand"].append(card)
+
+    for i in range(2):
+        card = deck.pop(0)
+        dealer["hand"].append(card)
+    print(calculate_hand_value(player["hand"]))
+    print(calculate_hand_value(dealer["hand"]))
+
 
 def dealer_play(deck: list[dict], dealer: dict) -> bool:
-    pass
+    while dealer["hand"] <= 17:
+        card = deck.pop(0)
+        dealer["hand"].append(card)
+        if dealer["hand"] > 21:
+            print("disqualification!")
+            return False
+    return True
+
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from core.deck import build_standard_deck, shuffle_by_suit
+from core.game_logi import calculate_hand_value, deal_two_each
 
 deck = build_standard_deck()
 game_desk = shuffle_by_suit(deck)
+player = {"hand":[]}
+dealer = {"hand":[]}
 print(game_desk)
 print(len(deck))
+deal_two_each(deck, player, dealer)
